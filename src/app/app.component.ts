b1a45b023917df574b7ad70b62ce068fd5c1b122
rohan <rohanjdhv550@gmail.com>
2022-07-08 17:34:20 +0530

    Angular material demoonly Important once added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+export interface TableData{
+	possition: number,
+	name: string,
+	age: number,
+	address: string,
+	phno: number
+}
+
+const TABLE_DATA:TableData[]=[
+	{possition:1,name:'Rohan',address:'Bangaluru',phno:8861681649,age:24},
+	{possition:2,name:'Rahul',address:'Bangaluru',phno:7411193649,age:32},
+	{possition:3,name:'Chetan',address:'Belagavi',phno:8879897654,age:28},
+	{possition:4,name:'Manoj',address:'Mangaluru',phno:7411197654,age:21}
+];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-material';
+	title = 'angular-material';
+	hidden:boolean = true;
+	count:number = 0;
+	form = new FormControl(
+
+	);
+	
+	options:any[] = [1,2,3,4]
+	
+	constructor(private dialog:MatDialog){
+		
+	}
+
+	toggleBage():void{
+		this.count++;
+		if(this.count==0)
+		{
+			this.hidden=true;
+		}
+		else
+		{
+			this.hidden = false;
+		}
+	}
+
+	openDialog():void{
+		const DilogRef = this.dialog.open(DialogExample,{
+			width:'20vw'
+		})
+		setTimeout(()=>{DilogRef.close()},2000)
+	}
+
+	//table data
+	TABLE_HEADER:string[]=['possition','name','address','phno','age'];
+	dataSource:TableData[] = TABLE_DATA;
+	
 }
+
+@Component({
+	selector: 'dialog-example',
+	templateUrl: 'dialog-example.html',
+})
+
+export class DialogExample{
+	constructor(public dialogRef: MatDialogRef<DialogExample>){
+	
+	}
+
+	onNoClick():void{
+		this.dialogRef.close();
+	}
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +22,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatAutocompleteModule,
+    MatOptionModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatBadgeModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
